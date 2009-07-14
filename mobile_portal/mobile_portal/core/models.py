0fb07728d6376a4c8be04550deeb9e97a0756b5f
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2009-07-14 15:34:34 +0000

    Updated apache.conf to put mobile_portal at beta/ Updated mobile_portal.settings to serve media at beta/site-media/ Started adding RSS feed handling.

--- mobile_portal/mobile_portal/core/models.py
+++ mobile_portal/mobile_portal/core/models.py
@@ -1,6 +1,9 @@
+import random
+from os.path import exists, join
 import simplejson
 from datetime import datetime, timedelta
 from django.db import models
+import django.dispatch
 
 class RecentManager(models.Manager):
     def get_query_set(self):
@@ -27,4 +30,30 @@ class Placemarks(models.Model):
     def save(self, force_insert=False, force_update=False):
         print self.data
         self._data = simplejson.dumps(self.data)
-        super(Placemarks, self).save()       
+        super(Placemarks, self).save()
+
+feed_fetched = django.dispatch.Signal(providing_args=['slug'])
+
+class Feed(models.Model):
+    slug = models.SlugField()
+    url = models.URLField()
+    fetch_period = models.PositiveIntegerField()
+    last_fetched = models.DateTimeField()
+    path = models.TextField(blank = True)
+    
+    def fetch(self):
+        while not self.path or exists(self.get_path()):
+            self.path = "%08x" % randint(0, 16**8-1)
+        if self.last_fetched + timedelta(0, self.fetch_period) < datetime.now():
+            require_save = True
+            f = open(self.path, 'w')
+            f.write(urllib.urlopen(self.url).read())
+            f.close()
+            self.last_fetched = datetime.now()
+            self.save()
+            
+            feed_fetched.send(sender=self, slug=self.slug)
+    
+    def get_path(self):
+        return join(settings.FEED_PATH, self.path)
+           
