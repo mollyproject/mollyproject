e5f5257eb9c9147bc91748b79b349e8c847d98c4
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2009-07-27 10:52:03 +0000

    Tidied up mobile_portal.results.urls

--- mobile_portal/mobile_portal/results/urls.py
+++ mobile_portal/mobile_portal/results/urls.py
@@ -1,20 +1,5 @@
 from django.conf.urls.defaults import *
-from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('mobile_portal.results.views',
-    # Example:
-    # (r'^mobile_portal/', include('mobile_portal.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
-    (r'^$', 'index', {}, 'results_index'),
+   (r'^$', 'index', {}, 'results_index'),
 )
-
