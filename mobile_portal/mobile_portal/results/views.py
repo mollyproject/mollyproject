13fc04306d27ddc6130d155e98d8587e3dcb630d
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2009-09-10 13:51:56 +0000

    Added try-except to catch weird twitter errors.

--- mobile_portal/mobile_portal/results/views.py
+++ mobile_portal/mobile_portal/results/views.py
@@ -6,17 +6,21 @@ from mobile_portal.core.renderers import mobile_render
 RSS_FEED = 'http://twitter.com/statuses/user_timeline/46711686.rss'
 RESULT_RE = re.compile(r"(?P<code>[A-Z]+) \((?P<title>.+)\)")
 def index(request):
-    xml = ET.fromstring(urllib.urlopen(RSS_FEED).read())
-    x_items = xml.findall('.//item')
-    items = []
-    for x_item in x_items:
-        match = RESULT_RE.search(x_item.find('title').text)
-        f = x_item.find('title').text
-        items.append({
-            'pubDate': datetime.date.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(x_item.find('pubDate').text))),
-            'code': match.groups()[0],
-            'title': match.groups()[1],
-        })
+    try:
+        xml = ET.fromstring(urllib.urlopen(RSS_FEED).read())
+        x_items = xml.findall('.//item')
+        items = []
+        for x_item in x_items:
+            match = RESULT_RE.search(x_item.find('title').text)
+            f = x_item.find('title').text
+            items.append({
+                'pubDate': datetime.date.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(x_item.find('pubDate').text))),
+                'code': match.groups()[0],
+                'title': match.groups()[1],
+            })
+    except:
+        items = []
+        
     context = {
         'items': items,
     }
