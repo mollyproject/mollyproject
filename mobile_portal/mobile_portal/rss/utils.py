b94077a13e88561afbe8f8e8c083d7e8bcd9ee8c
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2009-10-04 22:10:28 +0000

    Fixing some brokenness. Now encapsulates documents parts not in block tags with p tags.

--- mobile_portal/mobile_portal/rss/utils.py
+++ mobile_portal/mobile_portal/rss/utils.py
@@ -1,6 +1,8 @@
 from StringIO import StringIO
 from xml.sax.saxutils import escape
 import ElementSoup as ES
+from xml.etree.ElementTree import Element
+from xml.etree import ElementTree as ET
 
 VALID_TAGS = set([
     'a', 'abbr', 'address', 'bdo', 'big', 'blockquote', 'br', 'caption', 'cite',
@@ -8,6 +10,18 @@ VALID_TAGS = set([
     'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'img', 'ins', 'kbd', 'li', 'ol', 'p', 'pre',
     'q', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'table', 'tbody',
     'td', 'tfoot', 'th', 'thead', 'tr', 'tt', 'ul', 'var',
+])
+
+REMOVE_IF_EMPTY = set([
+    'a', 'abbr', 'address', 'bdo', 'big', 'blockquote', 'caption', 'cite',
+    'code', 'dd,' 'del', 'div', 'dfn', 'dl', 'dt', 'em', 'h1', 'h2', 'h3', 'h4',
+    'h5', 'h6', 'ins', 'kbd', 'li', 'ol', 'p', 'pre', 'q', 'samp', 'small',
+    'span', 'strong', 'sub', 'sup', 'tt', 'ul', 'var',
+])
+
+BLOCK_TAGS = set([
+    'blockquote', 'div', 'dl', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'ol', 'p',
+    'pre', 'table', 'ul',
 ])
 
 DROP_TAGS = set(['b', 'u', 'i', 'html'])
@@ -33,22 +47,50 @@ class HtmlSanitiser(object):
         html = (u"<html>%s</html>" % html).encode('utf8')
         html = ES.parse(StringIO(html), encoding='utf8')
 
-        return self.sanitise_node(html)
+        self.sanitise_node(html)
+        self.encapsulate_text_nodes(html)
 
+        return ET.tostring(html)[6:-7]
+
+    def encapsulate_text_nodes(self, html):
+    
+        html[0:0] = [Element('p')]
+        html[0].text, html.text = html.text, None
+        
+        i = 1
+        while i < len(html):
+            node = html[i]
+            
+            if node.tag in BLOCK_TAGS:
+                if not (html[i-1].text or len(html[i-1])):
+                    html[i-1:i], i = [], i-1
+                html[i+1:i+1] = [Element('p')]
+                print node.tag, i, ET.tostring(html)
+                html[i+1].text, node.tail = node.tail, None
+                
+                i += 2 
+            else:
+                html[i-1].append(node)
+                html[i:i+1] = []
+                
     def sanitise_node(self, node):
-        if node.tag in DROP_TAGS:
-            ret = [node.text or '']
-            for n in node:
-                ret.append(self.sanitise_node(n))
-                ret.append(n.tail or '')
-            return ''.join(ret)
-
-        if not node.tag in VALID_TAGS:
-            return ''
-
+        node[0:0], i = [Element('start')], 1
+        while i < len(node):
+            n = node[i]
+            if n.tag in DROP_TAGS:
+                if node[-1].tail:
+                    node[-1].tail += n.text
+                else:
+                    node[-1].tail = n.text
+                node[i:i+1] = n.getchildren()
+            elif not n.tag in VALID_TAGS:
+                node[i:i+1] = []
+            else:
+                i += 1
+                
         for attrib in set(node.attrib):
             if not attrib in VALID_ATTRIBS:
-                del node.attribs[attrib]
+                del node.attrib[attrib]
                 continue
             method_name = 'attrib_' + attrib
             if hasattr(self, method_name):
@@ -58,21 +100,22 @@ class HtmlSanitiser(object):
         if hasattr(self, method_name):
             getattr(self, method_name)(node)
 
-        if not node.tag:
-            return ''
+        for n in node[1:]:
+            self.sanitise_node(n)
 
-        if len(node.attrib):
-            ret = ['<%s %s>' % (node.tag, ' '.join(('%s="%s"' % (k, escape(v)) for (k, v) in node.attrib.items())))]
+        i = 1
+        while i < len(node):
+            n = node[i]
+            if node.tag in REMOVE_IF_EMPTY and len(n) == 0 and n.text == None:
+                node[i:i+1] = []
+            else:
+                i += 1
+
+        if node.text:
+            node.text += node[0].tail or ''
         else:
-            ret = ['<%s>' % node.tag]
-        ret.append(node.text or '')
-        for n in node:
-            ret.append(self.sanitise_node(n))
-            ret.append(n.tail or '')
-        ret.append('</%s>' % node.tag)
-        if len(ret) == 3 and ret[1] == '':
-            ret = [ret[0][:-1]+'/>']
-        return ''.join(ret) 
+            node.text = node[0].tail or None
+        node[0:1] = []
 
     def attrib_colspan(self, node, value):
         try:
@@ -97,7 +140,7 @@ class HtmlSanitiser(object):
         while value in self.seen_ids:
             value.append('-')
         self.seen_ids.add(value)
-        self.attrib['id'] = '%s-%s' % (self.id_prefix, value)
+        node.attrib['id'] = '%s-%s' % (self.id_prefix, value)
 
     def attrib_rowspan(self, node, value):
         try:
@@ -113,7 +156,7 @@ class HtmlSanitiser(object):
         for attrib in set(['dir', 'lang', 'xml:lang']):
             if attrib in node.attrib:
                 del node.attrib[attrib]
-        node[:] = []
+        node[1:] = []
         node.text = ''
     tag_hr = tag_br
     
@@ -128,8 +171,18 @@ class HtmlSanitiser(object):
 def sanitise_html(html):
     hs = HtmlSanitiser(html, id_prefix='s', header_offset=0)
     ret = hs.get_sanitised()
+    print '='*80
+    print html.encode('utf8')
+    print '-'*80
+    print ret.encode('utf8')
     return ret
 
 if __name__ == '__main__':
-    hs = HtmlSanitiser('<a href="foo">Hello<script>foo</script></p>')
+    hs = HtmlSanitiser("""<em>Squeak</em> Hello <strong>there</strong>,<br/>How are you?""")
+
+    print '='*80
+    print hs.html
+    print '-'*80
     print hs.get_sanitised()
+    print '='*80
+    
