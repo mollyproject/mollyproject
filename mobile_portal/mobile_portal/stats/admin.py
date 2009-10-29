a836eb1102cc943af3d64852deeed0e9a3d16931
Tim Fernando <tim.fernando@oucs.ox.ac.uk>
2009-10-29 16:43:22 +0000

    Playing with admin interface

--- mobile_portal/mobile_portal/stats/admin.py
+++ mobile_portal/mobile_portal/stats/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import Hit
 
 class HitAdmin(admin.ModelAdmin):
-    list_display = ('user', 'device_id', 'requested', 'view_name', 'status_code', 'response_time', 'full_path')
+    list_display = ('user', 'device_id', 'requested', 'status_code', 'response_time', 'full_path')
     list_filter = ('location_method', 'view_name', 'status_code', 'response_time')
-    search_fields = ('device_id')
+
 admin.site.register(Hit, HitAdmin)
