af34d23e1d10ecdca302b7c49222c362fcaeb61c
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-01-20 15:23:25 +0000

    Fix unicode/malformed HTML errors for feed item descriptions.

--- tags/0.2/mobile_portal/mobile_portal/rss/models.py
+++ tags/0.2/mobile_portal/mobile_portal/rss/models.py
@@ -2,6 +2,11 @@ from pytz import utc, timezone
 from django.contrib.gis.db import models
 from django.core.urlresolvers import reverse
 from xml.etree import ElementTree as ET
+import ElementSoup as ES
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 from mobile_portal.core.utils import resize_external_image
 from mobile_portal.oxpoints.models import Entity
 
@@ -62,13 +67,14 @@ class RSSItem(models.Model):
         
         
     def get_description_display(self, device):
-        html = ET.fromstring('<html>%s</html>' % self.description)
+        print type('<html>%s</html>' % self.description)
+        html = ES.parse(StringIO(('<html>%s</html>' % self.description).encode('utf8')))
         for img in html.findall('.//img'):
             eis = resize_external_image(img.attrib['src'], device.max_image_width-40)
             img.attrib['src'] = eis.get_absolute_url()
             img.attrib['width'] = '%d' % eis.width
             img.attrib['height'] = '%d' % eis.height
-        return ET.tostring(html)[6:-7]
+        return ET.tostring(html)[6:-7].decode('utf8')
             
         
     
