753ad47b52953a078f4c86f62baaa12fff5be763
Tim Fernando <tim.fernando@oucs.ox.ac.uk>
2010-01-21 12:54:31 +0000

    Added time delta template tag to find length in minutes

--- trunk/mobile_portal/mobile_portal/sakai/templatetags/sakai.py
+++ trunk/mobile_portal/mobile_portal/sakai/templatetags/sakai.py
@@ -7,3 +7,6 @@ register = template.Library()
 @register.filter
 def from_sakai_timestamp(value):
     return datetime.fromtimestamp(value/1000)
+
+def time_delta(value):
+    return (value['end'] - value['start']).seconds/60
