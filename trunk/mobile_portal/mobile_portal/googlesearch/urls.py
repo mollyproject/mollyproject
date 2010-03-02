f2714942911b13f30618f74079412e410e2c2852
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-03-02 12:27:42 +0000

    Tidying after pulling out some Oxford-specific bits

--- trunk/mobile_portal/mobile_portal/googlesearch/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-from views import GoogleSearchView
-
-urlpatterns = patterns('',
-    (r'^$', GoogleSearchView, {}, 'googlesearch_index'),
-)
--- trunk/mobile_portal/mobile_portal/weather/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from views import IndexView
-
-urlpatterns = patterns('mobile_portal.weather.views',
-    (r'^$', IndexView, {}, 'weather_index'),
-)
-
