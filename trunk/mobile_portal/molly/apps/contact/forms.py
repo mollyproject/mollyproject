1afa019dd5e325761948237014462c07c4926d60
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-03-03 15:15:59 +0000

    Bringing contact app in line with the documentation

--- trunk/mobile_portal/molly/apps/contact/forms.py
+++ trunk/mobile_portal/molly/apps/contact/forms.py
@@ -1,10 +1,4 @@
 from django import forms
 
-METHOD_CHOICES = (
-    ('phone', 'phone'),
-    ('email', 'email'),
-)
-
 class GenericContactForm(forms.Form):
     query = forms.CharField()
-    medium = forms.ChoiceField(choices = METHOD_CHOICES)
--- trunk/mobile_portal/molly/apps/contact/views.py
+++ trunk/mobile_portal/molly/apps/contact/views.py
@@ -9,11 +9,25 @@ from molly.utils.breadcrumbs import *
 from .forms import GenericContactForm
 
 class IndexView(BaseView):
+    @BreadcrumbFactory
+    def breadcrumb(cls, request, context):
+        return Breadcrumb(
+            'contact',
+            None,
+            'Contact search',
+            lazy_reverse('contact:index'),
+        )
+
     def initial_context(cls, request):
         return {
-            'method': 'phone' if request.GET.get('method')=='phone' else 'email',
-            'query': request.GET.get('q', ''),
+            'form': cls.conf.provider.form(request.GET or None),
+            'medium_choices': cls.conf.provider.medium_choices,
         }
+
+    def handle_GET(cls, request, context):
+        return mobile_render(request, context, 'contact/index')
+
+class ResultListView(IndexView):
 
     @BreadcrumbFactory
     def breadcrumb(cls, request, context):
@@ -27,19 +41,17 @@ class IndexView(BaseView):
     def handle_GET(cls, request, context):
         provider = cls.conf.provider
 
-        if hasattr(provider, 'form') and provider.form:
-            form = provider.form(request.GET or None)
-        else:
-            form = GenericContactForm(request.GET or None)
+        form = provider.form(request.GET or None)
+        medium = request.GET.get('medium', provider.medium_choices[0][0])
 
-        if form.is_valid():
+        if form.is_valid() and medium in [m[0] for m in provider.medium_choices]:
 
             try:
                 page = int(request.GET.get('page', '1'))
             except ValueError:
                 page = 1
 
-            query = provider.normalize_query(form.cleaned_data)
+            query = provider.normalize_query(form.cleaned_data, medium)
 
             if provider.handles_pagination:
                 paginator = provider.perform_query(page=page, **query)
@@ -57,6 +69,7 @@ class IndexView(BaseView):
             context.update({
                 'page': page,
                 'paginator': paginator,
+                'medium': medium,
             })
 
         if 'format' in request.GET and request.GET['format'] == 'json':
@@ -70,12 +83,31 @@ class IndexView(BaseView):
             return response
         else:
             context['form'] = form
-            return mobile_render(request, context, 'contact/index')
+            return mobile_render(request, context, 'contact/result_list')
 
     def handle_error(cls, request, context, message):
         context.update({
             'message': message,
         })
 
-        return mobile_render(request, context, 'contact/index')
+        return mobile_render(request, context, 'contact/result_list')
 
+
+class ResultDetailView(BaseView):
+    @BreadcrumbFactory
+    def breadcrumb(cls, request, context):
+        return Breadcrumb(
+            'contact',
+            None,
+            'Contact search',
+            lazy_reverse('contact:index'),
+        )
+
+
+    def handle_GET(cls, request, context, id):
+        try:
+            context['result'] = cls.conf.provider.fetch_result(id)
+        except BaseContactProvider.NoSuchResult:
+            raise Http404
+
+        return mobile_render(request, context, 'context/result_detail')
