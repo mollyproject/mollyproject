754dca0133c7386012bd88e3731abdc79a4a6a3f
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-03-23 13:03:32 +0000

    (Belatedly) added search form template tag.

---
+++ branches/extracting_oxford/molly/molly/apps/search/templatetags/molly_apps_search.py
@@ -0,0 +1,23 @@
+from django import template
+
+from molly.apps.search.forms import SearchForm
+
+register = template.Library()
+
+@register.tag
+def search_form(parser, token):
+    return SearchFormNode(*token.split_contents()[1:])
+    
+class SearchFormNode(template.Node):
+    """
+    Adds a SearchForm instance to the context.
+    """
+    
+    def __init__(self, prefix=None, name="search_form"):
+        self.prefix = prefix
+        self.name = name
+        
+    def render(self, context):
+        context[self.name] = SearchForm(prefix=self.prefix)
+        return ''
+    
