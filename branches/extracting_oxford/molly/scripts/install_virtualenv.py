ccf01b2eca1d0af26f435a8c9ac922c08c0a5114
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-04-28 10:21:17 +0100

    Made the installer script less verbose.

--- branches/extracting_oxford/molly/scripts/install_virtualenv.py
+++ branches/extracting_oxford/molly/scripts/install_virtualenv.py
@@ -2,18 +2,7 @@
 
 import os, sys, os.path, subprocess, shutil
 
-def main(source_path, deploy_path):
-    if os.path.exists(deploy_path):
-        print "Cannot deploy to path - already exists"
-        return 1
-
-    requirements = [l[:-1] for l in open(os.path.join(source_path, "requirements", "core.txt")) if l[:-1]]
-    print requirements
-
-    subprocess.call(["virtualenv", "--no-site-packages", deploy_path])
-    subprocess.call(["pip", "install", "-U", "-E", deploy_path] + requirements)
-    subprocess.call([os.path.join(deploy_path, "bin", "python"), os.path.join(source_path, "setup.py"), "install"])
-
+def cairo_hack(source_path, deploy_path):
     # This is a hack to create a file that should have been created at the pip stage.
     cairo_init_path = os.path.join(
         deploy_path, "lib", "python%d.%d" % sys.version_info[:2],
@@ -24,11 +13,63 @@ def main(source_path, deploy_path):
         f.write("from _cairo import *\n")
         f.close()
 
+def copy_demos(source_path, deploy_path):
     # Copy the demos across
     shutil.copytree(
         os.path.join(source_path, 'demos'),
         os.path.join(deploy_path, 'demos'),
     )
+   
+
+def main(source_path, deploy_path):
+    if os.path.exists(deploy_path):
+        print "Cannot deploy to path - already exists"
+        return 1
+
+    commands = [
+        ('Creating', 'virtual environment', ["virtualenv", "--no-site-packages", deploy_path]),
+    ]
+
+    requirements = [l[:-1] for l in open(os.path.join(source_path, "requirements", "core.txt")) if l[:-1]]
+    for requirement in requirements:
+        commands.append(
+            ('Installing', requirement,
+             ["pip", "install", "-U", "-E", deploy_path, requirement])
+        )
+
+    commands += [
+        ('Deploying', 'molly',
+         [os.path.join(deploy_path, "bin", "python"), os.path.join(source_path, "setup.py"), "install"]),
+        ('Tidying', 'cairo', cairo_hack),
+        ('Copying', 'demos', copy_demos),
+    ]
+
+
+    stdout_log = open('molly.stdout.log', 'w')
+    stderr_log = open('molly.stderr.log', 'w')
+    succeeded = True
+    for i, (action, item, command) in enumerate(commands):
+        print "%s %s (%2d/%2d)" % (action[:12].ljust(12), item[:40].ljust(40), (i+1), len(commands)),
+        if callable(command):
+            try:
+                return_code = command(source_path, deploy_path) or 0
+            except Exception:
+                return_code = 1
+        else:
+            return_code = subprocess.call(command, stdout=stdout_log, stderr=stderr_log)
+        print "[%s]" % ('FAILED' if return_code else '  OK  ')
+        succeeded == succeeded and (return_code == 0)
+
+    if succeeded:
+        print """
+Molly was successfully installed to %(deploy_path)s.
+The following command will take you inside your virtualenv:
+
+$ source %(activate)s""" % {
+            'deploy_path': deploy_path,
+            'activate': os.path.join(deploy_path, "bin", "activate"),
+        }
+  
 
 
 if __name__ == '__main__':
