dffb1a4eb338a3bfe546cd041e56b38bfda57d52
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-05-04 15:20:43 +0100

    Cloudmade geolocation provider fixed to return lat-longs the right way round.

--- branches/extracting_oxford/molly/molly/contrib/generic/providers/cloudmade.py
+++ branches/extracting_oxford/molly/molly/contrib/generic/providers/cloudmade.py
@@ -13,20 +13,20 @@ logger = logging.getLogger('molly.contrib.generic.cloudmade')
 class CloudmadeGeolocationProvider(BaseGeolocationProvider):
     REVERSE_GEOCODE_URL = 'http://geocoding.cloudmade.com/%(api_key)s/geocoding/closest/road/%(lat)f,%(lon)f.js'
     GEOCODE_URL = 'http://geocoding.cloudmade.com/%(api_key)s/geocoding/find/%(query)s.js'
-    
+
     def __init__(self, search_locality):
         self.search_locality = search_locality
-    
+
     def reverse_geocode(self, lon, lat):
-    
+
         params = {
             'api_key': settings.API_KEYS['cloudmade'],
             'lon': lon,
             'lat': lat,
         }
-    
+
         data = urllib.urlopen(self.REVERSE_GEOCODE_URL % params)
-        
+
         print self.REVERSE_GEOCODE_URL % params
         json = simplejson.load(data)
         if not json:
@@ -37,21 +37,21 @@ class CloudmadeGeolocationProvider(BaseGeolocationProvider):
                 'location': (lon, lat),
                 'accuracy': 100,
             }]
-        
+
     def geocode(self, query):
-    
+
         if self.search_locality and not (', ' in query or ' near ' in query):
             query += ', %s' % self.search_locality
-            
+
         query = query.strip()
         if query.split(' ')[0][0].isdigit():
             query = ' '.join(query.split(' ')[1:])
-        
+
         params = {
             'api_key': settings.API_KEYS['cloudmade'],
             'query': urllib.quote_plus(query),
         }
-        
+
         try:
             request_url = self.GEOCODE_URL % params
             response = urllib.urlopen(request_url)
@@ -62,20 +62,22 @@ class CloudmadeGeolocationProvider(BaseGeolocationProvider):
         except Exception,e:
             raise
             return []
-        
+
         if not json:
             return []
-    
+
         results = []
         for feature in json['features']:
             try:
-                bounds_a, bounds_b = [Point(p, srid=4326).transform(settings.SRID, clone=True) for p in feature['bounds']]
+                # Cloudmade returns a lat-long (and we use long-lats internally)
+                bounds_a, bounds_b = [Point(p[1], p[0], srid=4326).transform(settings.SRID, clone=True) for p in feature['bounds']]
             except OGRException:
                 # The point wasn't transformable into the co-ordinate
                 # scheme desired - it's probably a long way away.
                 continue
-                
+
             centroid = tuple(feature['centroid']['coordinates'])
+            centroid = centroid[1], centroid[0]
             accuracy = bounds_a.distance(bounds_b) / 1.414
             try:
                 results.append({
@@ -86,5 +88,4 @@ class CloudmadeGeolocationProvider(BaseGeolocationProvider):
             except KeyError:
                 results += self.reverse_geocode(*centroid)
 
-        
         return results
