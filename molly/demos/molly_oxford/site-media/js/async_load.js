518facb95f0a244b04ab7a62ac32ee1738002473
necaris <necaris@gmail.com>
2010-05-12 12:39:33 -0700

    added minified version of async loader script

--- molly/demos/molly_oxford/site-media/js/async_load.js
+++ molly/demos/molly_oxford/site-media/js/async_load.js
@@ -23,20 +23,20 @@ function to_absolute(url) {
 
 // Callback method that swaps in the asynchronously loaded bits to the page, and fades it in
 function load_callback(data, textStatus, xhr) {
-    $('body').html(data.body);
-    $('title').html(data.title);
-    $('.content').html(data.content);
-    $('#loading').hide()
+    jQuery('body').html(data.body);
+    jQuery('title').html(data.title);
+    jQuery('.content').html(data.content);
+    jQuery('#loading').hide()
     capture_outbound();
-    $('body').fadeTo('fast', 1);
+    jQuery('body').fadeTo('fast', 1);
 }
 
 function ajax_load(url, query, meth) {
     query['format'] = 'fragment';
-    console.log("Async loading " + url);
-    console.log("    aka " + to_absolute(url));
-    console.log("    with " + query);
-    console.log("    meth " + meth);
+    // console.log("Async loading " + url);
+    // console.log("    aka " + to_absolute(url));
+    // console.log("    with " + query);
+    // console.log("    meth " + meth);
     var settings = {'url': to_absolute(url), 'data': query, 'type': meth, 'dataType': 'json'};
     settings['success'] = function(data, textStatus, xhr) {
         var abs_url = to_absolute(url);
@@ -45,31 +45,31 @@ function ajax_load(url, query, meth) {
         window.location.hash = current_url;
         return load_callback(data, textStatus, xhr);
     };
-    $.ajax(settings);
-    $('body').fadeTo('fast', 0.1);
-    $('#loading').css({'position': 'fixed', 'top': '30%', 'left': '50%', 'opacity': '0.8'}).show()
-    $('#loading').fadeIn('fast');
+    jQuery.ajax(settings);
+    jQuery('body').fadeTo('fast', 0.1);
+    jQuery('#loading').css({'position': 'fixed', 'top': '30%', 'left': '50%', 'opacity': '0.8'}).show()
+    jQuery('#loading').fadeIn('fast');
     return false;
 }
 
 function capture_outbound()  {
     // Intercept all forms
-    $('form').submit(function(evt) {
-            var serial = $(this).serializeArray();
+    jQuery('form').submit(function(evt) {
+            var serial = jQuery(this).serializeArray();
             var datamap = {}
             var i = 0;
             for (i = 0; i < serial.length; i++) {
                 datamap[serial[i].name] = serial[i].value;
             }
-            return ajax_load($(this).attr('action'), datamap, $(this).attr('method'));
+            return ajax_load(jQuery(this).attr('action'), datamap, jQuery(this).attr('method'));
         });
     // Intercept all links with an href
-    $('a[href]').click(function(evt) {
-            return ajax_load($(this).attr('href'), {}, 'GET');
+    jQuery('a[href]').click(function(evt) {
+            return ajax_load(jQuery(this).attr('href'), {}, 'GET');
         });
 }
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
     if (window.location.hash != current_url) {
         console.log("Hash mismatch! " + window.location.hash + " != " + current_url + "! Reloading...");
         ajax_load(window.location.hash.substr(1), {}, "GET");
