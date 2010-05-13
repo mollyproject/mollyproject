7a21e614213dc589dbf22e61a5ecbd0183cb4f59
necaris <necaris@gmail.com>
2010-05-12 17:49:14 -0700

    Added all elements to cache manifest; added loading background css to site-media/groups/smart.css; added Javascript to flash error message on failed async loads; added code to scroll to top when loading a new page

--- molly/demos/molly_oxford/site-media/js/async_load.js
+++ molly/demos/molly_oxford/site-media/js/async_load.js
@@ -24,19 +24,22 @@ function to_absolute(url) {
 // Callback method that swaps in the asynchronously loaded bits to the page, and fades it in
 function async_load_callback(data, textStatus, xhr) {
     jQuery('body').html(data.body);
+    jQuery('#loading-bg').css({'opacity': 0.75}).show();
     jQuery('title').html(data.title);
     jQuery('.content').html(data.content);
-    jQuery('#loading').hide()
     capture_outbound();
-    jQuery('body').fadeTo('fast', 1);
+    jQuery('#loading-bg').fadeTo('fast', 0, function() {
+        jQuery('#loading-bg').hide();
+        jQuery('html, body').animate({'scrollTop': 0}, 100);
+    });
 }
 
 function async_load(url, query, meth) {
     query['format'] = 'fragment';
-    console.log("Async loading " + url);
+    /* console.log("Async loading " + url);
     console.log("    aka " + to_absolute(url));
     console.log("    with " + query);
-    console.log("    meth " + meth);
+    console.log("    meth " + meth); */
     var settings = {'url': to_absolute(url), 'data': query, 'type': meth, 'dataType': 'json'};
 
     settings['success'] = function(data, textStatus, xhr) {
@@ -47,14 +50,20 @@ function async_load(url, query, meth) {
         return async_load_callback(data, textStatus, xhr);
     };
     settings['error'] = function(xhr, textStatus, errorThrown) {
-        jQuery('#loading').html("<p>Uh-oh! There was an error loading the page - please try again.</p>");
-        jQuery('#loading').css({'top': '30%', 'text-align': 'center', 'opacity': 1});
+        jQuery('#loading-bg')
+            .html('<p style="position:fixed; top: 10%; width:100%; margin:0 auto; text-align:center;">Error loading page - please try again.</p>')
+            .css({'font-size': '20px', 'font-weight': 'bold'})
+            .fadeTo('fast', 0.9, function() {
+                setTimeout(function() {
+                    jQuery('#loading-bg').fadeTo('fast', 0, function () {
+                        jQuery('#loading-bg').hide();
+                    });
+                }, 1200);
+            });
     };
 
     jQuery.ajax(settings);
-    jQuery('body').fadeTo('fast', 0.1);
-    jQuery('#loading').css({'position': 'fixed', 'top': '30%', 'left': '50%', 'opacity': 0.1}).show()
-    jQuery('#loading').fadeTo('fast', 1);
+    jQuery('#loading-bg').show().fadeTo('fast', 0.75)
     return false;
 }
 
@@ -69,16 +78,18 @@ function capture_outbound()  {
             }
             return async_load(jQuery(this).attr('action'), datamap, jQuery(this).attr('method'));
         });
+    console.log("Captured outbound forms");
     // Intercept all links with an href
     jQuery('a[href]').click(function(evt) {
             return async_load(jQuery(this).attr('href'), {}, 'GET');
         });
+    console.log("Captured outbound links");
 }
 
 jQuery(document).ready(function() {
     if (window.location.hash != current_url) {
         console.log("Hash mismatch! " + window.location.hash + " != " + current_url + "! Reloading...");
-        ajax_load(window.location.hash.substr(1), {}, "GET");
+        async_load(window.location.hash.substr(1), {}, "GET");
     }
     capture_outbound();
 });
