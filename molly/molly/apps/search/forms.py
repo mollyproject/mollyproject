f15f22d7594c39a202522c467da717c6ad0a4f04
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-06-10 12:51:50 +0100

    Applications for validating in the search form are now picked up when a SearchForm is initialized, not at server start-up (removing a circular import dependency).

--- molly/molly/apps/search/forms.py
+++ molly/molly/apps/search/forms.py
@@ -1,15 +1,27 @@
 from django import forms
 from molly.conf import all_apps
 
-APPLICATION_CHOICES = (
-    ('', 'Show all'),
-) + tuple((app.local_name, app.title) for app in all_apps())
+class SearchForm(object):
+    def __new__(cls, *args, **kwargs):
+        try:
+            return cls._search_form_class(*args, **kwargs)
+        except AttributeError:
+            cls._search_form_class = cls._get_search_form_class()
+            return cls._search_form_class(*args, **kwargs)
 
-class SearchForm(forms.Form):
-    query = forms.CharField(label='Search')
-    application = forms.ChoiceField(
-        label='Filter',
-        widget=forms.HiddenInput(),
-        choices=APPLICATION_CHOICES,
-        required=False,
-    )
+    @classmethod
+    def _get_search_form_class(cls):
+        APPLICATION_CHOICES = (
+            ('', 'Show all'),
+        ) + tuple((app.local_name, app.title) for app in all_apps() if app.application_name)
+
+        class SearchForm(forms.Form):
+            query = forms.CharField(label='Search')
+            application = forms.ChoiceField(
+                label='Filter',
+                widget=forms.HiddenInput(),
+                choices=APPLICATION_CHOICES,
+                required=False,
+            )
+
+        return SearchForm
