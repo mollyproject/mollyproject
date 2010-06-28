c79f90be9a4dcaaa3e8fc7521e1f713898bffacd
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-06-28 14:27:49 +0100

    Tidied up display of flag status; added timesince filter since last updateed.

--- molly/demos/molly_oxford/apps/river_status/models.py
+++ molly/demos/molly_oxford/apps/river_status/models.py
@@ -12,6 +12,7 @@ class FlagStatus(models.Model):
     order = models.IntegerField()
     name = models.TextField()
     status = models.CharField(max_length=6, choices=FLAG_STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
 
     def get_icon_url(self):
         return "http://www.ourcs.org.uk/files/image/icons/flag_%s.png" % self.status
