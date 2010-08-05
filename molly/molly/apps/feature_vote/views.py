f933efdf57c6ebd809bc9e9ec323378e6f76c1f3
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-08-05 16:22:22 +0100

    Added up and down voting.

--- molly/molly/apps/feature_vote/views.py
+++ molly/molly/apps/feature_vote/views.py
@@ -1,5 +1,8 @@
+import random
+
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden
 
 from molly.utils.views import BaseView
 from molly.utils.breadcrumbs import *
@@ -9,6 +12,14 @@ from .models import Idea
 from .forms import IdeaForm
 
 class IndexView(BaseView):
+
+    # A mapping from (old, new) to (delta down, delta up)
+    vote_transitions = {
+        (-1,-1) : ( 0, 0),  ( 0,-1): (+1, 0),  (+1,-1): (+1,-1),
+        (-1, 0) : (-1, 0),  ( 0, 0): ( 0, 0),  (+1, 0): ( 0,-1),
+        (-1,+1) : (-1,+1),  ( 0,+1): ( 0,+1),  (+1,+1): ( 0, 0),
+    }
+
     @BreadcrumbFactory
     def breadcrumb(cls, request, context):
         return Breadcrumb(
@@ -19,9 +30,14 @@ class IndexView(BaseView):
         )
 
     def initial_context(cls, request):
+        if not 'feature_vote:csrf' in request.session:
+            request.session['feature_vote:csrf'] = ''.join(random.choice('0123456789abcdef') for i in range(8))
+        if not 'feature_vote:votes' in request.session:
+            request.session['feature_vote:votes'] = {}
         return {
             'ideas': Idea.objects.all(),
             'form': IdeaForm(request.POST or None),
+            'csrf' : request.session['feature_vote:csrf'],
         }
 
     def handle_GET(cls, request, context):
@@ -30,11 +46,30 @@ class IndexView(BaseView):
     def handle_POST(cls, request, context):
         form = context['form']
 
+        if request.POST.get('csrf') != request.session['feature_vote:csrf']:
+            return HttpResponseForbidden()
+
+        if 'vote_up' in request.POST or 'vote_down' in request.POST:
+            idea = get_object_or_404(Idea, id = request.POST.get('id', 0))
+            previous_vote = request.session['feature_vote:votes'].get(idea.id, 0)
+            vote = 1 if 'vote_up' in request.POST else -1
+            request.session['feature_vote:votes'][idea.id] = vote
+            request.session.modified = True
+
+            idea.down_vote += cls.vote_transitions[(previous_vote, vote)][0]
+            idea.up_vote   += cls.vote_transitions[(previous_vote, vote)][1]
+
+            idea.save()
+
+            return HttpResponseSeeOther(reverse('feature_vote:index'))
+
         if form.is_valid():
             form.save()
             return HttpResponseSeeOther(reverse('feature_vote:index'))
         else:
             return cls.handle_GET(request, context)
+
+
 
 class IdeaDetailView(BaseView):
     @BreadcrumbFactory
