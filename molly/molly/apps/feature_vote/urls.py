a899a4c7b6451d0681da904fcb4b047301a41b25
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-08-05 11:23:37 +0100

    Added template and url for new IdeaDetailView.

--- molly/molly/apps/feature_vote/urls.py
+++ molly/molly/apps/feature_vote/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
-from .views import IndexView
+from .views import IndexView, IdeaDetailView
 
 urlpatterns = patterns('',
     (r'^$', IndexView, {}, 'index'),
+    (r'^(?P<id>\d+)/$', IdeaDetailView, {}, 'idea-detail'),
 )
