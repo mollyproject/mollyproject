b29130b1c62af3e6f2aadc756732f3cec3abf40a
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-08-05 11:14:01 +0100

    Added an IdeaDetailView.

--- molly/molly/apps/feature_vote/views.py
+++ molly/molly/apps/feature_vote/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from molly.utils.views import BaseView
 from molly.utils.breadcrumbs import *
 
@@ -21,3 +23,20 @@ class IndexView(BaseView):
     def handle_GET(cls, request, context):
         return cls.render(request, context, 'feature_voting/index')
 
+class IdeaDetail(BaseView):
+    @BreadcrumbFactory
+    def breadcrumb(cls, request, context, id):
+        return Breadcrumb(
+            cls.conf.local_name,
+            lazy_parent(IndexView),
+            context['idea'].title,
+            lazy_reverse('feature_voting:idea-detail'),
+        )
+
+    def initial_context(cls, request, id):
+        return {
+            'idea': get_object_or_404(Idea, id=id),
+        }
+
+    def handle_GET(cls, request, context, id):
+        return cls.render(request, context, 'feature_voting/idea_detail')
