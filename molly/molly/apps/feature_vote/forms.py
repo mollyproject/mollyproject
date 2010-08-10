d403b8ac3f5058c80c5e0619001a46c580330847
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-08-10 15:22:04 +0100

    Added verbose_name and label attributes to feature_vote model and form.

--- molly/molly/apps/feature_vote/forms.py
+++ molly/molly/apps/feature_vote/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from models import Feature
 
 class FeatureForm(forms.ModelForm):
-    user_name = forms.CharField(widget=forms.TextInput())
-    title = forms.CharField(widget=forms.TextInput())
+    user_name = forms.CharField(widget=forms.TextInput(), label="Your name")
+    title = forms.CharField(widget=forms.TextInput(), label="Feature title")
 
     class Meta:
         fields = ('user_name', 'user_email', 'title', 'description')
--- molly/molly/apps/feature_vote/models.py
+++ molly/molly/apps/feature_vote/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 class Feature(models.Model):
-    user_name = models.TextField()
-    user_email = models.EmailField()
+    user_name = models.TextField(verbose_name='Your name')
+    user_email = models.EmailField(verbose_name='E-mail address')
 
-    title = models.TextField()
-    description = models.TextField()
+    title = models.TextField(verbose_name='Feature title')
+    description = models.TextField(verbose_name='Description')
 
     up_vote = models.IntegerField(default=0)
     down_vote = models.IntegerField(default=0)
