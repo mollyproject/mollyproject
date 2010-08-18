5c418235489de91b9fb7170605ed996f77ea02da
Tim Fernando <tim.fernando@oucs.ox.ac.uk>
2010-08-18 14:21:24 +0100

    Adding icons and more changes to tutorial styles

--- molly/molly/apps/sakai/templatetags/sakai.py
+++ molly/molly/apps/sakai/templatetags/sakai.py
@@ -11,3 +11,20 @@ def from_sakai_timestamp(value):
 @register.filter    
 def places_left(value):
     return (value["maxNoOfAttendees"]-len(value["attendees"]))
+
+@register.filter
+def signup_status(ts, event):
+    if ts["signedUp"]:
+        return 'signed-up'
+    elif not places_left(ts):
+        if event["allowWaitList"]:
+            
+            return 'waiting-list'
+        else:
+            return 'full'
+    else:
+        return 'available'
+
+@register.filter
+def signup_status_human(value):
+    return {'signed-up': 'Signed Up', 'waiting-list': 'Waiting List', 'full': 'Full', 'available': 'Available'}[value]
