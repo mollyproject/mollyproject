8a3e249c50e57aa0b0b3f7fb2bebeb93cff6b532
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-09-30 15:50:07 +0100

    Removed BlogArticle cruft from the olden days.

--- molly/molly/apps/home/models.py
+++ molly/molly/apps/home/models.py
@@ -4,11 +4,6 @@ class Config(models.Model):
     key = models.SlugField()
     value = models.TextField()
 
-class BlogArticle(models.Model):
-    updated = models.DateTimeField()
-    html = models.TextField()
-    guid = models.TextField()
-
 class UserMessage(models.Model):
     session_key = models.TextField()
     message = models.TextField()
