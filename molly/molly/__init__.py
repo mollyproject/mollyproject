88cf15ffe1a54dd3051cae9b2dc08468cb527f15
Tim Fernando <tim.fernando@oucs.ox.ac.uk>
2010-11-09 15:38:34 +0000

    Moved molly/molly to molly (removed one level of folders)

--- molly/molly/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Molly Project
-http://mollyproject.org
-A framework for creating Mobile Web applications for HE/FE institutions.
-Originally as deployed by the University of Oxford as 'Mobile Oxford' at http://m.ox.ac.uk
-"""
-
-import init_logging
