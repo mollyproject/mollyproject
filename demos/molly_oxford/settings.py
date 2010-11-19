43ee9e1d78ead024e5e9e93e9c6deea1d293e4c6
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2010-11-19 10:49:53 +0000

    Use a fork of django-compress to get the required effect when handling off-site SSL media

--- demos/molly_oxford/settings.py
+++ demos/molly_oxford/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE_CLASSES = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.debug',
+    'django.core.context_processors.request',
 #    'django.core.context_processors.i18n',
     'molly.utils.context_processors.ssl_media',
 #    'django.contrib.messages.context_processors.messages',
