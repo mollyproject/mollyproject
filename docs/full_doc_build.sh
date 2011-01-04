7e40dcbf013c1ffbbfbb4786c3dda7c9de80d28a
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-01-04 14:58:56 +0000

    Also generate epydoc documentation

--- docs/full_doc_build.sh
+++ docs/full_doc_build.sh
@@ -4,12 +4,18 @@ function build_documentation {
     tag=$1
     rm -rf build
     make html
+    API_TEMP=`mktemp -d`
     rm -rf $OUTPUT_DIR/$tag/
-    mkdir $OUTPUT_DIR/$tag/
+    mkdir -p $OUTPUT_DIR/$tag/
     cp -rf build/html/* $OUTPUT_DIR/$tag/
+    
+    epydoc -o $API_TEMP --html --graph=all -n Molly -u http://mollyproject.org/ --no-private ../molly/
+    rm -rf $OUTPUT_DIR/api/$tag/
+    mkdir -p $OUTPUT_DIR/api/$tag/
+    cp -rf $API_TEMP/* $OUTPUT_DIR/api/$tag/
+    rm -rf $API_TEMP
 }
 
-REPO=git://github.com/mollyproject/mollyproject.git
 OUTPUT_DIR=$1
 
 if [ -z "$OUTPUT_DIR" ] ; then
