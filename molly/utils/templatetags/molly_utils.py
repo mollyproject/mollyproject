1cf3313bb1be24eeeef3802a8e8b6d7f9d765b67
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-01-27 11:16:14 +0000

    Make templatetag naming consistent

--- molly/utils/templatetags/molly_utils.py
+++ molly/utils/templatetags/molly_utils.py
@@ -5,10 +5,15 @@ except ImportError:
 
 from lxml import etree
 
+from datetime import datetime
+
 from django import template
-from django.utils.safestring import SafeUnicode
+from django.utils.safestring import SafeUnicode, mark_safe
 
 from molly.utils.xslt import transform
+from molly.wurfl import device_parents
+from molly.apps.places import get_entity
+from molly.apps.places.models import Entity
 
 register = template.Library()
 
@@ -24,3 +29,83 @@ def sanitize_html(value, args=''):
         'class_prefix': class_prefix,
     })
     return SafeUnicode(etree.tostring(document)[6:-7])
+
+@register.filter(name="gte")
+def gte(value, arg):
+    return value >= float(arg)
+
+@register.filter(name="lte")
+def lte(value, arg):
+    return value <= arg
+
+@register.filter(name="contains")
+def contains(value, arg):
+    return arg in value
+
+@register.filter
+def this_year(value, arg=None):
+    if not arg:
+        arg = datetime.now()
+    return value.year == arg.year
+
+UNUSUAL_NUMBERS = {
+    '+448454647': '0845 46 47',
+    '+448457909090': '08457 90 90 90'
+}
+
+@register.filter(name="telephone")
+def telephone(value, arg):
+    value = value.replace(" ", "").replace("-", "")
+    if value.startswith("0"):
+        value = "+44" + value[1:]
+
+    normalised = value
+
+    if normalised in UNUSUAL_NUMBERS:
+        value = UNUSUAL_NUMBERS[normalised]
+    else:
+        if value.startswith("+44"):
+            value = "0" + value[3:]
+
+        for dialing_code in ['01865', '0845']:
+            if value.startswith(dialing_code):
+                value = dialing_code + " " + value[len(dialing_code):]
+
+        if value.startswith('01865 2'):
+            value = "01865 (2)" + value[7:]
+
+    if arg == 'nolink':
+        return value
+    else:
+        return mark_safe('<a href="tel:%s">%s</a>' % (normalised, value))
+
+@register.filter
+def telephone_uri(value):
+    value = value.replace(" ", "").replace('-','')
+    if value.startswith("0"):
+        value = "+44" + value[1:]
+    value = "tel:" + value
+
+    return value
+
+@register.filter(name="device_has_parent")
+def device_has_parent(value, arg):
+    if not value:
+        return False
+    return arg in device_parents[value.devid]
+
+@register.filter
+def header_width(value):
+    try:
+        value = int(value)
+        if value < 160:
+            return 128
+        elif value < 240:
+            return 160
+    except ValueError, e:
+        pass
+    return 128
+
+@register.filter('get_entity')
+def get_entity_filter(value):
+    return get_entity(*value)
