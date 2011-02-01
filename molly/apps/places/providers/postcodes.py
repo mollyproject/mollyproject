cd5f824a2d756c8770be6f47d946c7e39c85228e
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-02-01 15:34:12 +0000

    Fix postcode importing all

--- molly/apps/places/providers/postcodes.py
+++ molly/apps/places/providers/postcodes.py
@@ -28,7 +28,7 @@ class PostcodesMapsProvider(BaseMapsProvider):
         if self.import_areas:
             filenames = ['Code-Point Open/data/CSV/%s.csv' % code.lower() for code in self.import_areas]
         else:
-            filenames = [path for path in archive.listnames() if re.match(r'Code\-Point Open\/data\/CSV\/[a-z]{1,2}.csv', path)]
+            filenames = [path for path in archive.namelist() if re.match(r'Code\-Point Open\/data\/CSV\/[a-z]{1,2}.csv', path)]
 
         for filename in filenames:
             if hasattr(archive, 'open'):
