10fc4f65627cf3812490b2a38641b3138978d366
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-02-25 13:52:56 +0000

    Config generator should include favourites app

--- scripts/config_generator.py
+++ scripts/config_generator.py
@@ -726,6 +726,13 @@ APPLICATIONS = [
         ),
     """
     
+    # Favourites - always
+    config += """
+    Application('molly.favourites', 'favourites', 'Favourite pages',
+        display_to_user = False,
+    ),
+    """
+
     config += """
 ]
 """
