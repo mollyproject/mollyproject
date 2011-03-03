6c488d267cd5919eb545855a522d5cd7ec7d0fec
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-03-03 15:19:26 +0000

    Revert "Don't cache markers, admin files or uncompressed JS/CSS"
    This reverts commit 357d4053e80e433b899ecacc15fba2a04cd6032b.

--- molly/utils/management/commands/generate_cache_manifest.py
+++ molly/utils/management/commands/generate_cache_manifest.py
@@ -16,14 +16,6 @@ class Command(NoArgsCommand):
             print >>cache_manifest, "CACHE MANIFEST"
             print >>cache_manifest, "CACHE:"
             for root, dirs, files in os.walk(settings.STATIC_ROOT):
-                if root == settings.STATIC_ROOT:
-                    # Don't cache admin media or markers
-                    dirs.remove('admin')
-                    dirs.remove('markers')
                 url = '/'.join(root.split(os.sep)[static_prefix_length:])
                 for file in files:
-                    # Don't cache uncompressed JS/CSS
-                    _, ext = os.path.splitext(file)
-                    if ext in ('.js','.css') and 'c' != url.split('/')[0]:
-                        continue
                     print >>cache_manifest, "%s%s/%s" % (settings.STATIC_URL, url, file)
