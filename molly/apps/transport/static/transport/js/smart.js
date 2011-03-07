0c154c06324085519b3d4aa3fa34192fed919b41
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-03-07 12:47:03 +0000

    AJAX updating of park and ride data

--- molly/apps/transport/static/transport/js/smart.js
+++ molly/apps/transport/static/transport/js/smart.js
@@ -1,5 +1,25 @@
 function refreshTransport(data){
-    window.location.reload()
+    $('#park_and_rides .section-content').empty()
+    for (var i in data.park_and_rides) {
+        var entity = data.park_and_rides[i]
+        $('#park_and_rides .section-content').append('<div class="park-and-ride"><h3><a href="' + entity._url + '">' + entity.title + '</a></h3></div>')
+        if (entity.metadata.park_and_ride) {
+            if (entity.metadata.park_and_ride.unavailable) {
+                spaces = '?'
+                $('.park-and-ride:last').append('<p><em>Space information currently unavailable</em></p>')
+            } else {
+                $('.park-and-ride:last').append('<div class="capacity-bar"><div style="width: ' + entity.metadata.park_and_ride.percentage.toString() + '%; height:7px;background-color: #960300;">&nbsp;</div></div>')
+                spaces = entity.metadata.park_and_ride.spaces.toString()
+            }
+            $('.park-and-ride:last').append('<p>Spaces: ' + spaces + ' / ' + entity.metadata.park_and_ride.capacity + '</p>')
+        }
+        if (i < data.park_and_rides.length - 1 || i%2 == 0) {
+            $('.park-and-ride:last').css('float', 'left')
+        }
+        if (i%2 == 0) {
+            $('.park-and-ride:last').css('clear', 'left')
+        }
+    }
 }
 
 function ajaxTransportUpdate(){
