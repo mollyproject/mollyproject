ff3d479e431da0f094fad9dc138f18b5f0c7bcb4
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-03-18 16:29:16 +0000

    Make epydoc run inside the Virtualenv

--- docs/full_doc_build.sh
+++ docs/full_doc_build.sh
@@ -9,7 +9,7 @@ function build_documentation {
     mkdir -p $OUTPUT_DIR/$tag/
     cp -rf build/html/* $OUTPUT_DIR/$tag/
     
-    epydoc -o $API_TEMP --html --graph=all -n Molly -u http://mollyproject.org/ --no-private ../molly/
+    $PYTHON `which epydoc` -o $API_TEMP --html --graph=all -n Molly -u http://mollyproject.org/ --no-private ../molly/
     rm -rf $OUTPUT_DIR/api/$tag/
     mkdir -p $OUTPUT_DIR/api/$tag/
     cp -rf $API_TEMP/* $OUTPUT_DIR/api/$tag/
