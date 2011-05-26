a35f8980830882311892a766eefd2018325c2064
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-05-26 13:33:17 +0100

    Migrate webcam names to i18n

--- molly/apps/webcams/admin.py
+++ molly/apps/webcams/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from models import Webcam
+from models import Webcam, WebcamName
 
-class WeatherAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'credit',)
+class WebcamNameInline(admin.TabularInline):
+    model = WebcamName
+    fk_name = "webcam"
 
-admin.site.register(Webcam, WeatherAdmin)
+
+class WebcamAdmin(admin.ModelAdmin):
+    list_display = ('description', 'credit',)
+    inlines = [
+        WebcamNameInline,
+    ]
+
+
+admin.site.register(Webcam, WebcamAdmin)
--- molly/apps/webcams/models.py
+++ molly/apps/webcams/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
+from molly.utils.i18n import name_in_language
+
 WEBCAM_WIDTHS = (100, 300, 200)
 
 class Webcam(models.Model):
     slug = models.SlugField()
     url = models.URLField()
     fetch_period = models.PositiveIntegerField()
-    title = models.TextField()
     description = models.TextField(null=True)
     credit = models.TextField(null=True)
+    
+    @property
+    def title(self):
+        return name_in_language(self, 'title')
 
-    class Meta:
-        ordering = ('title',)
+
+class WebcamName(models.Model):
+    webcam = models.ForeignKey(Webcam, related_name='names')
+    language_code = models.CharField(max_length=10)
+    title = models.TextField()
