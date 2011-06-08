e2f6df50e3d138772503b29d49f6fcf489c7da43
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-06-08 17:32:43 +0100

    MOLLY-147: document feature vote app

--- molly/apps/feature_vote/models.py
+++ molly/apps/feature_vote/models.py
@@ -9,22 +9,42 @@ from molly.apps.home.models import UserMessage
 from molly.utils import send_email
 
 class Feature(models.Model):
+    
+    #: Name of the submitting user
     user_name = models.TextField(verbose_name=_('Your name'))
+    
+    #: E-mail address of the submitting user
     user_email = models.EmailField(verbose_name=_('E-mail address'))
-
+    
+    #: Title of the feature request
     title = models.TextField(verbose_name=_('Feature title'))
+    
+    #: More verbose description of the feature request
     description = models.TextField(verbose_name=_('Description'))
 
+    #: The number of up-votes this feature request has received
     up_vote = models.IntegerField(default=0)
+    
+    #: The number of down-votes this feature request has received
     down_vote = models.IntegerField(default=0)
 
+    #: The date this feature request was submitted
     created = models.DateTimeField(auto_now_add=True)
+    
+    #: The date this was last commented on
     last_commented = models.DateTimeField(blank=True, null=True)
-
+    
+    #: Whether or not this has been moderated to be displayed
     is_public = models.BooleanField(default=False)
+    
+    #: Whether or not this has been removed (e.g., after completion)
     is_removed = models.BooleanField(default=False)
+    
+    #: The date this feature was implemented
     implemented_on = models.DateTimeField(blank=True, null=True,
                                           verbose_name=_('Date this feature was implemented'))
+    
+    #: Whether or not notifications have been sent to users about if this has been implemented or not
     notifications_sent = models.BooleanField(default=False)
 
     class Meta:
@@ -68,6 +88,9 @@ class Feature(models.Model):
 
     @property
     def net_votes(self):
+        """
+        The combined number of votes for this suggestion
+        """
         return self.up_vote - self.down_vote
 
     def get_absolute_url(self):
