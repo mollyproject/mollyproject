98f588b7948ca410568f9277b29d8442148f0ba1
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-07-18 15:14:26 +0100

    Add save function to tours - also, optimise route and suggest entities

---
+++ molly/apps/tours/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from molly.apps.places.models import Entity
+
+class Tour(models.Model):
+    
+    stops = models.ManyToManyField(Entity)
+
+class StopOnTour(models.Model):
+    
+    tour = models.ForeignKey(Tour)
+    entity = models.ForeignKey(Entity)
+    order = models.IntegerField()
+    
+    class Meta:
+        ordering = ['order']
+
--- molly/apps/tours/views.py
+++ molly/apps/tours/views.py
@@ -1,11 +1,15 @@
+from django.core.urlresolvers import reverse
+from django.contrib.gis.measure import D
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
 from molly.apps.places import get_entity
-from molly.apps.places.models import EntityType
+from molly.apps.places.models import Entity, EntityType
 from molly.utils.breadcrumbs import *
 from molly.utils.views import BaseView
 from molly.wurfl import device_parents
-from molly.routing import generate_route
+from molly.routing import generate_route, optimise_points
+from molly.apps.tours.models import Tour, StopOnTour
 
 class IndexView(BaseView):
     
@@ -17,6 +21,16 @@ class IndexView(BaseView):
             _('Tours'),
             lazy_reverse('index'),
         )
+    
+    def initial_context(self, request):
+        context = super(IndexView, self).initial_context(request)
+        
+        if 'tours:visited' in request.session:
+            context.update({
+                'tours': Tour.objects.filter(id__in=request.session['tours:visited'])
+            })
+        
+        return context
     
     def handle_GET(self, request, context):
         return self.render(request, context, 'tours/index')
@@ -63,17 +77,38 @@ class SaveView(CreateView):
     
     def handle_GET(self, request, context, entities):
         
+        if len(context['entities']) < 2:
+            # Need at least 2 entities to be a tour
+            return self.bad_request(request)
+        
         # Now attempt to order entities optimally
+        if len(context['entities']) > 2 and len(context['entities']) <= 10:
+            context['entities'] = optimise_points([(entity, entity.location) for entity in context['entities']])
+            context['optimised_entities'] = True
         
         # Save back to database
+        tour = Tour.objects.create()
+        for i, entity in enumerate(context['entities']):
+            StopOnTour.objects.create(entity=entity, tour=tour, order=i)
         
         # Add any suggested "passing-by" entities to the context to be presented
         # back to the user
+        if hasattr(self.conf, 'suggested_entities'):
+            route = generate_route([e.location for e in context['entities']], 'foot')
+            suggestion_filter = Q()
+            for sv in self.conf.suggested_entities:
+                scheme, value = sv.split(':')
+                suggestion_filter |= Q(_identifiers__scheme=scheme,
+                                       _identifiers__value=value)
+            context['suggestions'] = Entity.objects.filter(
+                suggestion_filter,
+                location__distance_lt=(route['path'],
+                        D(m=getattr(self.conf, 'suggestion_distance', 100))))
+        
+        context['tour'] = tour
         
         return super(SaveView, self).handle_GET(request, context, entities)
 
-# TODO: need to have an "update view" for the use case of deciding that you
-# want to add a new entity
 
 # Todo: paper view
 
