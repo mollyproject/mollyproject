c2ede45b1a46fe76369fc1b128b9b767ce786058
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-07-18 10:56:01 +0100

    Add Cyclestreets API

--- molly/routing/providers/cyclestreets.py
+++ molly/routing/providers/cyclestreets.py
@@ -1,10 +1,20 @@
+from urllib import urlencode
 from urllib2 import urlopen
 import simplejson
 
 from django.conf import settings
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, LineString
+from django.utils.text import capfirst
+from django.utils.translation import ugettext as _
 
-raise ImportError()
+from molly.apps.places.models import bearing_to_compass
+from molly.utils.templatetags.molly_utils import humanise_distance, humanise_seconds
+
+CYCLESTREETS_URL = 'http://www.cyclestreets.net/api/journey.json?%s'
+
+if 'cyclestreets' not in settings.API_KEYS:
+    # Cyclestreets not configured
+    raise ImportError()
 
 def generate_route(points, type):
     """
@@ -31,4 +41,52 @@ def generate_route(points, type):
     @rtype: dict
     """
     
-    return {'error': 'Not written yet'}
+    # Build Cyclestreets request:
+    url = CYCLESTREETS_URL % urlencode({
+        'key': settings.API_KEYS['cyclestreets'],
+        'plan': 'balanced',
+        'itinerarypoints': '|'.join('%f,%f' % (p[0], p[1]) for p in points)
+    })
+    
+    json = simplejson.load(urlopen(url))
+    
+    if not json:
+        return {
+            'error': _('Unable to plot route')
+        }
+    else:
+        
+        summary = json['marker'][0]['@attributes']
+        
+        waypoints = []
+        for i, waypoint in enumerate(json['marker'][1:]):
+            segment = waypoint['@attributes']
+            waypoints.append({
+                'instruction': _('%(instruction)s at %(name)s') % {
+                        'instruction': capfirst(segment['turn']),
+                        'name': segment['name']
+                    },
+                'additional': _('%(direction)s for %(distance)s (taking approximately %(time)s)') % {
+                        'direction': bearing_to_compass(int(segment['startBearing'])),
+                        'distance': humanise_distance(segment['distance'], False),
+                        'time': humanise_seconds(segment['time'])
+                    },
+                'waypoint_type': {
+                        'straight on': 'straight',
+                        'turn left': 'left',
+                        'bear left': 'slight-left',
+                        'sharp left': 'sharp-left',
+                        'turn right': 'right',
+                        'bear right': 'slight-right',
+                        'sharp right': 'sharp-right',
+                        'double-back': 'turn-around',
+                    }.get(segment['turn']),
+                'location': Point(*map(float, segment['points'].split(' ')[0].split(',')))
+            })
+        
+        return {
+            'total_time': summary['time'],
+            'total_distance': summary['length'],
+            'waypoints': waypoints,
+            'path': LineString(map(lambda ps: Point(*map(float, ps.split(','))), summary['coordinates'].split(' ')))
+        }
