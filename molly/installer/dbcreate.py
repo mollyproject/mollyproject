2fbd90a9995e8552e818e53d3b213e4cfef470de
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-07-19 15:09:40 +0100

    Fix broken setting of postgres password

--- molly/installer/dbcreate.py
+++ molly/installer/dbcreate.py
@@ -3,6 +3,8 @@ Creates a database for Molly, and appropriate users, once given login
 information as super user, or by running as root.
 """
 
+import os
+
 from molly.installer.utils import quiet_exec, CommandFailed
 
 def create(dba_user, dba_pass, username, password, database):
