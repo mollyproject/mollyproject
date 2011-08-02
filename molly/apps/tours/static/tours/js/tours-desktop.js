fbd7ec6804097bebfff772ceb4099c94380cf505
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-08-02 17:04:18 +0100

    Add some JSy goodness to the desktop create page

---
+++ molly/apps/tours/static/tours/js/tours-desktop.js
@@ -0,0 +1,32 @@
+$(function(){
+    
+    function reset_selected_bindings(){
+        $('#poi-list li a').unbind('click');
+        $('#poi-list li a').click(function(e){
+            var entity = $(this).attr('class');
+            $(this).parent().remove();
+            $('.attractions-grouping .' + entity).parent().removeClass('selected');
+            entity = entity.replace('-', ':');
+            new_current_stops = [];
+            for (i in current_stops) {
+                if (current_stops[i] != entity) {
+                    new_current_stops.push(current_stops[i]);
+                }
+            }
+            current_stops = new_current_stops;
+            e.preventDefault();
+        })
+    }
+    reset_selected_bindings();
+    
+    $('.attractions-grouping li a').click(function(e){
+        var entity = $(this).attr('class');
+        $('#poi-list').append('<li><a href="#" class="' + entity + '">' + $(this).html() + '</a></li>');
+        $('.attractions-grouping .' + entity).parent().addClass('selected');
+        reset_selected_bindings();
+        entity = entity.replace('-', ':');
+        current_stops.push(entity);
+        e.preventDefault();
+    })
+    
+})
