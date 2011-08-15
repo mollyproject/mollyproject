4f83878e110da6074799ef1a2b9ba347aba8efef
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-08-15 17:17:57 +0100

    Use haversine to compute distances, rather than rely on transformations into local grids

--- molly/geolocation/__init__.py
+++ molly/geolocation/__init__.py
@@ -1,13 +1,16 @@
 from functools import wraps
+import logging
 
 from django.conf import settings
 from django.contrib.gis.geos import Point
 
 from molly.conf import get_app
-
+from molly.utils import haversine
 from molly.geolocation.models import Geocode
 
 __all__ = ['geocode', 'reverse_geocode']
+
+logger = logging.getLogger(__name__)
 
 def _cached(getargsfunc):
     def g(f):
@@ -16,8 +19,11 @@ def _cached(getargsfunc):
             args = getargsfunc(*args, **kwargs)
             app = get_app('molly.geolocation', args.pop('local_name', None))
             try:
-                return Geocode.recent.get(local_name=app.local_name, **args).results
+                geocode = Geocode.recent.get(local_name=app.local_name, **args)
+                logger.debug('Found cached geocode')
+                return geocode.results
             except Geocode.DoesNotExist:
+                logger.debug('Geocode not found in cache')
                 pass
             except Geocode.MultipleObjectsReturned:
                 Geocode.recent.filter(local_name=app.local_name, **args).delete()
@@ -30,23 +36,23 @@ def _cached(getargsfunc):
                     results[i:i+1] = []
                 else:
                     i += 1
-
+            
             if hasattr(app, 'prefer_results_near'):
                 point = Point(app.prefer_results_near[:2])
                 distance = app.prefer_results_near[2]
                 filtered_results = [
                     result for result in results if
-                        Point(result['location']).distance(point) <= distance]
+                        haversine(Point(result['location']), point) <= distance]
                 if filtered_results:
                     results = filtered_results
 
             try:
-                geocode, created = Geocode.objects.get_or_create(local_name = app.local_name,
-                                                            **args)
+                geocode, created = Geocode.objects.get_or_create(
+                    local_name=app.local_name, **args)
             except Geocode.MultipleObjectsReturned:
-                Geocode.objects.filter(local_name = app.local_name, **args).delete()
-                geocode, created = Geocode.objects.get_or_create(local_name = app.local_name,
-                                                            **args)
+                Geocode.objects.filter(local_name=app.local_name, **args).delete()
+                geocode, created = Geocode.objects.get_or_create(
+                    local_name=app.local_name, **args)
             geocode.results = results
             geocode.save()
             
@@ -54,14 +60,14 @@ def _cached(getargsfunc):
         return h
     return g
 
-@_cached(lambda query,local_name=None:{'query':query, 'local_name':local_name})
+@_cached(lambda query,local_name=None: {'query':query, 'local_name':local_name})
 def geocode(query, providers):
     results = []
     for provider in providers:
         results += provider.geocode(query)
     return results
 
-@_cached(lambda lon,lat,local_name=None:{'lon': lon, 'lat':lat, 'local_name':local_name})
+@_cached(lambda lon,lat,local_name=None: {'lon': lon, 'lat':lat, 'local_name':local_name})
 def reverse_geocode(lon, lat, providers):
     results = []
     for provider in providers:
