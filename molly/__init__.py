c24e1e2d06331d162b3257cd92a0b7fe5fd97c57
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-08-17 12:17:28 +0100

    Fix bad merge

--- molly/__init__.py
+++ molly/__init__.py
@@ -4,8 +4,4 @@ http://mollyproject.org
 A framework for creating Mobile Web applications for HE/FE institutions.
 """
 
-<<<<<<< HEAD
 __version__ = '1.3dev'
-=======
-__version__ = '1.2.2'
->>>>>>> master
