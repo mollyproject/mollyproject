0c3ad9dc3945351c31d37914584f5a1c1cd305e7
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-08-18 14:19:23 +0100

    MOLLY-194: Add new 'build_all_media' management commands

--- molly/batch_processing/management/commands/create_crontab.py
+++ molly/batch_processing/management/commands/create_crontab.py
@@ -1,3 +1,5 @@
+from StringIO import StringIO
+from subprocess import Popen, PIPE
 import sys
 
 from django.core.management.base import NoArgsCommand
@@ -6,11 +8,25 @@ from molly.batch_processing import create_crontab
 
 class Command(NoArgsCommand):
     help = "Generates a crontab"
-
+    
+    option_list = BaseCommand.option_list + (
+            make_option('--pipe-to-crontab',
+                action='store_true',
+                dest='pipe_to_crontab',
+                default=False,
+                help='Pipe the cron output directly to the crontab command'),
+            )
+    
     def handle(self, *args, **options):
-        if len(args) == 0:
+        if options['pipe_to_crontab']:
+            f = StringIO()
+        elif len(args) == 0:
             f = sys.stdout
         else:
             f = open(args[0], 'w')
-            
+        
         create_crontab(f)
+        
+        if options['pipe_to_crontab']:
+            cron = Popen('crontab', stdin=PIPE)
+            cron.communicate(input=f.getvalue())
---
+++ molly/utils/management/commands/build_all_media.py
@@ -0,0 +1,38 @@
+import os
+
+from django.core.management import call_command
+from django.core.management.base import NoArgsCommand
+
+class Command(NoArgsCommand):
+    
+    option_list = BaseCommand.option_list + (
+        make_option('--develop',
+            action='store_true',
+            dest='develop',
+            default=False,
+            help='Create symlinks, rather than copy, existing media, then start the dev server'),
+        ) + (
+        make_option('--skip-cron',
+            action='store_true',
+            dest='skip_cron',
+            default=False,
+            help='Skip creating a crontab'),
+        )
+    
+    def handle_noargs(self, skip_cron, develop, **options):
+        call_command('sync_and_migrate')
+        try:
+            from molly.wurfl import wurfl_data
+        except ImportError:
+            no_wurfl = True
+        else:
+            no_wurfl = False
+        if no_wurfl or not develop:
+            call_command('update_wurfl')
+        call_command('collectstatic', noinput=True, link=develop)
+        call_command('synccompress')
+        if not skip_cron:
+            call_command('create_crontab', pipe_to_crontab=(os.name != 'nt'))
+        if develop:
+            call_command('runserver')
+
