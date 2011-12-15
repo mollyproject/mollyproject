1736bc8486ee419771dc792f87a3d9bc3c2a3b1b
Tim Fernando <info@timfernando.com>
2011-12-15 12:15:03 +0000

    Changed the weather admin to allow filtering by observation and forecast. Removed the temperature filter which was a bit odd

--- molly/apps/weather/admin.py
+++ molly/apps/weather/admin.py
@@ -4,6 +4,6 @@ from models import Weather
 
 class WeatherAdmin(admin.ModelAdmin):
     list_display = ('observed_date', 'temperature', 'min_temperature', 'max_temperature', 'sunset', 'sunrise', 'wind_speed', 'wind_direction', 'humidity', 'pressure')
-    list_filter = ('temperature',)
+    list_filter = ('ptype')
     
 admin.site.register(Weather, WeatherAdmin)
