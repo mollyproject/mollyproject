7f8dee44de4d5a82485663805e6561bd0631ec82
Tim Fernando <info@timfernando.com>
2011-12-15 13:15:17 +0000

    Added extra description to weather app. Closes MOLLY-256

--- molly/apps/weather/__init__.py
+++ molly/apps/weather/__init__.py
@@ -4,4 +4,7 @@ Weather application
 * Stores a single (latest) observation
 * Stores historical forecast data (not well defined)
 * Can only handle ONE provider at once.
+
+The application could do with being reengineered. Especially for more
+real time data sources. The current object model is a tad muddled.
 """
