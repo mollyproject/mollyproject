051b175805b6805c35023642fcb6f915c4006ad0
Tim Fernando <info@timfernando.com>
2011-12-15 12:35:48 +0000

    Added a app description

---
+++ molly/apps/weather/__init__.py
@@ -0,0 +1,7 @@
+"""
+Weather application
+* Grabs weather from defined providers
+* Stores a single (latest) observation
+* Stores historical forecast data (not well defined)
+* Can only handle ONE provider at once.
+"""
