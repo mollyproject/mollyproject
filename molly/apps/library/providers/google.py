64b6c4e681e471683215014b8bbdfdd9c07b31ea
Martin Filliau <martin@filliau.com>
2011-12-16 15:41:00 +0000

    correcting pdf link in google books provider

--- molly/apps/library/providers/google.py
+++ molly/apps/library/providers/google.py
@@ -11,7 +11,7 @@ logger = getLogger(__name__)
 
 class GoogleBooksProvider(BaseMetadataProvider):
     
-    # There's a double % here, because this is doing to be sprintf'd twice -
+    # There's a double % here, because this is going to be sprintf'd twice -
     # once now, and then once later to build the query
     BOOK_SEARCH_ENDPOINT = 'https://www.googleapis.com/books/v1/volumes?q=%%s&key=%s' % settings.API_KEYS.get('google_books', '')
     
@@ -21,7 +21,7 @@ class GoogleBooksProvider(BaseMetadataProvider):
         @type books: [LibrarySearchResult]
         @rtype: None
         """
-        
+
         for book in books:
             # ISBNs from OLIS aren't always well-formed
             for isbn in [isbn.split()[0] for isbn in book.isbns]:
@@ -30,13 +30,12 @@ class GoogleBooksProvider(BaseMetadataProvider):
                         urlopen(self.BOOK_SEARCH_ENDPOINT % quote_plus(isbn)))
                     result = results['items'][0]
                 except HTTPError, KeyError:
-                    logger.info('Google Books lookup failed', exc_info=True)
+			logger.info('Google Books lookup failed', exc_info=True)
                 else:
-                    
                     # Get the image
                     image_links = result.get('volumeInfo',{}
                                              ).get('imageLinks', [])
-                    
+
                     # Find the biggest one we have as a basis for resize
                     for image_size in ('extraLarge', 'large', 'medium',
                                        'small', 'thumbnail',
@@ -44,15 +43,15 @@ class GoogleBooksProvider(BaseMetadataProvider):
                         if image_size in image_links:
                             book.cover_image = image_links[image_size]
                             break
-                    
+
                     # Annotate epub link
                     book.epub = result.get('accessInfo', {}).get(
                         'epub', {}).get('downloadLink')
-                    
+
                     # Annotate PDF link
-                    book.epub = result.get('accessInfo', {}).get(
+                    book.pdf = result.get('accessInfo', {}).get(
                         'pdf', {}).get('downloadLink')
-                    
+
                     # This ISBN got us a cover image, don't look up anymore
                     if hasattr(book, 'cover_image'):
                         break
