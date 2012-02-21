b09bb93b0f637431eb27b76f0f77f3fc2b4a93a0
Martin Filliau <martin@filliau.com>
2012-02-21 14:32:58 +0000

    excluding entity field from feeds admin as it causes "poor" performances

--- molly/apps/feeds/admin.py
+++ molly/apps/feeds/admin.py
@@ -3,7 +3,10 @@ from models import Feed, Item
 
 class FeedAdmin(admin.ModelAdmin):
     list_display = ('title', 'entity', 'rss_url')
-    
+    # excluded because not used and causes performance
+    # problems when you have 47622 entities...
+    exclude = ('entity',)
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'last_modified', 'feed')
     list_filter = ('feed', 'last_modified')
