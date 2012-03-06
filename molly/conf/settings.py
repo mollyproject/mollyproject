1caafe1830660d704231154414b07e7ab7193ed9
Martin Filliau <martin@filliau.com>
2012-03-06 13:28:56 +0000

    only use molly logging if LOGGING_CONFIG is set to None

--- molly/conf/settings.py
+++ molly/conf/settings.py
@@ -1,9 +1,9 @@
 import imp
 
 from django.utils.importlib import import_module
-from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import include as urlconf_include
 from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
+from django.conf import settings
 
 """
 Provides a framework for Molly application objects.
@@ -101,12 +101,13 @@ class Application(object):
 
         # Configure any logging for this application, passing the
         # configuration lest it needs it.
-        try:
-            logconfig = import_module(self.application_name + '.logconfig')
-        except ImportError, e:
-            pass
-        else:
-            logconfig.configure_logging(self.conf)
+        if settings.LOGGING_CONFIG is None:
+            try:
+                logconfig = import_module(self.application_name + '.logconfig')
+            except ImportError, e:
+                pass
+            else:
+                logconfig.configure_logging(self.conf)
 
         return self.conf
 
