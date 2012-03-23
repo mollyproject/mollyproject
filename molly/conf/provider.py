85a56949d3b1dbc190070802a8804bc66120439b
David King <dave@davbo.org>
2012-03-23 14:33:48 +0000

    Tasks now have the correct behaviour if called directly or called via async. Fixed a bug with setting class attributes on Provider.

--- molly/conf/provider.py
+++ molly/conf/provider.py
@@ -1,10 +1,7 @@
-from celery.task import PeriodicTask
+from celery.task import PeriodicTask, Task
 
 
 class Provider(object):
-    def __init__(self, *args, **kwargs):
-        self.class_path = None
-        self.verbose_name = None
 
     def __call__(self):
         return self
@@ -14,26 +11,44 @@ class Provider(object):
         """Constructor, looks for decorated tasks and registers them with Celery,
         this is done in a non-standard way as Celery tasks can only be functions, not methods.
         """
-        class_path = "%s.%s" % (cls.__module__, cls.__name__)
         ins = cls(*args, **kwargs)
-        ins.class_path = class_path
-        for name, fun in cls.__dict__.items():
+        for attr_name in dir(cls):
+            if callable(getattr(cls, attr_name)):
+                fun = getattr(cls, attr_name)
+            else:
+                continue
             if hasattr(fun, 'task'):
+                # This is a decorated method
                 run_every = fun.task['run_every']
-                name = "%s.%s" % (class_path, name)
-                def run(self, **kwargs):
-                    meth = getattr(self.provider, self.name.split('.')[-1])
-                    return meth(**self.metadata)
-                def init(self, provider=ins, run_every=run_every, metadata=fun.task['initial_metadata']):
+                name = "%s.%s.%s" % (cls.__module__, cls.__name__, attr_name)
+                new_attr_name = '__task_%s' % attr_name
+                if run_every:
+                    base = PeriodicTask
+                    def run(self, **kwargs):
+                        meth = getattr(self.provider, self.true_method)
+                        return meth(**self.metadata)
+                else:
+                    base = Task
+                    def run(self, *args, **kwargs):
+                        meth = getattr(self.provider, self.true_method)
+                        return meth(*args, **kwargs)
+                def __call__(self, *args, **kwargs):
+                    meth = getattr(self.provider, self.true_method)
+                    return meth(*args, **kwargs)
+                def __init__(self, provider=ins, run_every=run_every,
+                        metadata=fun.task['initial_metadata'], base=base):
                     self.provider = provider
                     self.run_every = run_every
                     self.metadata = metadata
-                    super(BatchTask, self).__init__()
-                type(name, (BatchTask,), {'__init__': init,
+                t = type(name, (base,), {'__init__': __init__,
+                    '__call__': __call__,
                     'run': run,
                     '__module__': cls.__module__,
                     'name': name,
+                    'true_method': new_attr_name,
                     })
+                setattr(ins, new_attr_name, getattr(ins, attr_name))
+                setattr(ins, attr_name, t())
         return ins
 
 
@@ -42,12 +57,15 @@ class BatchTask(PeriodicTask):
     cache of metadata.
     """
     abstract = True
+
     def after_return(self, status, value, *args, **kwargs):
-        if value: self.metadata = value
+        if value:
+            self.metadata = value
+
 
 def task(run_every=None, initial_metadata={}):
     def dec(fun):
         fun.task = {'run_every': run_every,
-                'initial_metadata':initial_metadata}
+                'initial_metadata': initial_metadata}
         return fun
     return dec
