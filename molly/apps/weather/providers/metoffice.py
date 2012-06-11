2e6024897f8780bfce257fd9da469133250339e1
Martin Filliau <martin@filliau.com>
2012-06-11 18:05:52 +0100

    MetOffice forecasts working.

--- molly/apps/weather/providers/metoffice.py
+++ molly/apps/weather/providers/metoffice.py
@@ -1,12 +1,12 @@
 import logging
 from urllib2 import urlopen
 from lxml import etree
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta, date, time
 
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-from molly.conf.providers import Provider, task
+from molly.conf.provider import Provider, task
 from molly.apps.weather.models import Weather
 from molly.apps.weather.models import (
     Weather, OUTLOOK_CHOICES, VISIBILITY_CHOICES, PRESSURE_STATE_CHOICES,
@@ -53,15 +53,15 @@ METOFFICE_OUTLOOK_CHOICES = (
     (32, 'unk'),   # NOT USED?
     (33, 'h'),   # Haze
     )
-METOFFICE_VISIBILITY_CHOICES = (
-    ('UN', 0),      # "Unknown", TODO missing association
-    ('VP', VISIBILITY_CHOICES['vp']),
-    ('PO', VISIBILITY_CHOICES['p']),
-    ('MO', VISIBILITY_CHOICES['m']),
-    ('GO', VISIBILITY_CHOICES['g']),
-    ('VG', VISIBILITY_CHOICES['vg']),
-    ('EX', VISIBILITY_CHOICES['e']),
-)
+#METOFFICE_VISIBILITY_CHOICES = (
+#    ('UN', 0),      # "Unknown", TODO missing association
+#    ('VP', VISIBILITY_CHOICES['vp']),
+#    ('PO', VISIBILITY_CHOICES['p']),
+#    ('MO', VISIBILITY_CHOICES['m']),
+#    ('GO', VISIBILITY_CHOICES['g']),
+#    ('VG', VISIBILITY_CHOICES['vg']),
+#    ('EX', VISIBILITY_CHOICES['e']),
+#)
 
 BASE_METOFFICE_URL = "http://partner.metoffice.gov.uk/public/val"
 
@@ -97,26 +97,39 @@ class MetOfficeProvider(Provider):
     def import_forecasts(self, **metadata):
         api = ApiWrapper()
         forecasts = api.get_daily_forecasts_by_location(self.forecasts_location_id)
+        for fc in forecasts:
+            f = forecasts[fc]
+            forecast, created = Weather.objects.get_or_create(
+                    location_id=self.forecasts_location_id,
+                    observed_date=fc,
+                    ptype=PTYPE_FORECAST)
+            forecast.min_temperature = float(f['Night']['Nm'])
+            forecast.max_temperature = float(f['Day']['Dm'])
+            outlooks = dict(METOFFICE_OUTLOOK_CHOICES)
+            forecast.outlook = outlooks[int(f['Day']['W'])]
+            forecast.observed_date = datetime.combine(fc, time(hour=0))
+            forecast.save()
 
     @task(run_every=timedelta(minutes=15))
     def import_observation(self, **metadata):
         api = ApiWrapper()
         observations = api.get_observations_by_location(self.observations_location_id)
-        latest_day = self.sortdict(observations)[-1]
-        latest_hour = self.sortdict(latest_day)[-1]
-        observation = Weather.objects.get_or_create(
-            location_id=self.observations_location_id, ptype=PTYPE_OBSERVATION)
-        observation.temperature = int(latest_hour['T'])
-        observation.wind_speed = int(latest_hour['S'])
-        observation.wind_direction = latest_hour['D']
-        observation.pressure = int(latest_hour['P'])
-        #observation.observed_date =
-        observation.outlook = METOFFICE_OUTLOOK_CHOICES[latest_hour['W']]
+        latest_day = sorted(observations)[-1]
+        latest_hour = sorted(observations[latest_day])[-1]
+        latest = observations[latest_day][latest_hour]
+        observation, created = Weather.objects.get_or_create(
+            location_id=self.observations_location_id,
+            ptype=PTYPE_OBSERVATION)
+        observation.temperature = float(latest['T'])
+        observation.wind_speed = int(latest['S'])
+        observation.wind_direction = latest['D']
+        observation.pressure = int(latest['P'])
+        observation.observed_date = datetime.combine(latest_day,
+                time(int(latest_hour)/60))
+        outlooks = dict(METOFFICE_OUTLOOK_CHOICES)
+        observation.outlook = outlooks[int(latest['W'])]
         #observation.humidity = not available??
         observation.save()
-
-    def sortdict(self, d):
-        for key in sorted(d): yield d[key]
 
 
 class ApiWrapper(object):
@@ -129,21 +142,21 @@ class ApiWrapper(object):
     OBSERVATIONS_FRAGMENT_URL = '/wxobs/all/xml'
 
     def get_daily_forecasts_by_location(self, location_id):
-        content = urlopen('{0}{1}/{2}?res=daily&key={3}'.format(
-            BASE_METOFFICE_URL,
-            self.FORECAST_FRAGMENT_URL,
-            location_id,
-            settings.API_KEYS['metoffice']
-        )).read()
+        url = '{0}{1}/{2}?res=daily&key={3}'.format(BASE_METOFFICE_URL,
+                self.FORECAST_FRAGMENT_URL, location_id, settings.API_KEYS['metoffice'])
+        logger.debug('MetOffice: %s' % url)
+        content = urlopen(url).read()
         return self.scrape_xml(content)
 
     def get_observations_by_location(self, location_id):
-        content = urlopen('{0}{1}/{2}?res=hourly&key={3}').format(
+        url = '{0}{1}/{2}?res=hourly&key={3}'.format(
             BASE_METOFFICE_URL,
             self.OBSERVATIONS_FRAGMENT_URL,
             location_id,
             settings.API_KEYS['metoffice']
-        ).read()
+        )
+        logger.debug('MetOffice: %s' % url)
+        content = urlopen(url).read()
         return self.scrape_xml(content)
 
     def scrape_xml(self, content):
